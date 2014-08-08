52f59a05ab2ce677051f49e90ad81444cd42c2d9
turza <muturza3.1416@gmail.com>
2014-08-08 16:50:13 +0200

    lr.cxx

--- LHEanalysis/lr.cxx
+++ LHEanalysis/lr.cxx
@@ -1,7 +1,8 @@
-void lr(std::string variable, int nbin, int low, int up ) //plotting the function
+void lr(std::string variable, std::string variable1, int nbin, int low, int up ) ////variable selects the variable to plot variable 1 selects the cut
+
 {
-TFile file("my.root","READ");
-TFile file1("my1.root","READ");
+TFile file("signal.root","READ");
+TFile file1("background.root","READ");
 
 TTree* tree=(TTree*) file.Get("tree");
 TTree* tree1=(TTree*) file1.Get("tree");
@@ -13,18 +14,19 @@ TH1F *h2 = new TH1F("h2", "h2 title", nbin, low, up);
 TString drawStr1(variable + " >> h1");
 TString drawStr2(variable + " >> h2");
 
-tree->Draw(drawStr1);
-tree1->Draw(drawStr2);
+tree->Draw(drawStr1, variable1.c_str());
+tree1->Draw(drawStr2, variable1.c_str());
+
 
 TCanvas *c = new TCanvas();
-h1->Draw();
-c->SaveAs("h1.pdf");
+//h1->DrawNormalized();
+//c->SaveAs("h1.pdf");
 
 
 
-c->Clear();
-h2->Draw();
-c->SaveAs("h2.pdf");
+//c->Clear();
+//h2->DrawNormalized();
+//c->SaveAs("h2.pdf");
 
 
        TH1F *hnew = (TH1F*)h1->Clone("hnew");
@@ -33,12 +35,12 @@ h1-> Divide(h2);
 
 c->Clear();
 c->SetLogy();
-hnew->Draw();
-hnew.SetLineColor(1); \\1->black
-h1->Draw("same");
-h1.SetLineColor(2); \\2->red
-h2->Draw("same");
-h2.SetLineColor(4); \\4-> blue
+hnew.SetLineColor(1); 
+hnew->DrawNormalized();
+h1.SetLineColor(2); 
+h1->DrawNormalized("same");
+h2.SetLineColor(4); 
+h2->DrawNormalized("same");
 
 c->SaveAs("ratio.pdf");
 
